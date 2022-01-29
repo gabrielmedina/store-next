5d927e667ec84207f18c18b421837a17193c3683
gabrielmedina <gmedina.santos@gmail.com>
2022-01-29 16:18:13 -0400

    feat: add header component

---
+++ components/index.ts
@@ -0,0 +1,5 @@
+import { Header } from './layout/header/header'
+
+export {
+  Header
+}
---
+++ components/layout/header/header.tsx
@@ -0,0 +1,8 @@
+
+export const Header = () => {
+  return (
+    <header>
+      <h1>Store</h1>
+    </header>
+  )
+}
