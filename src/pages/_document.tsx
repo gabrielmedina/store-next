5d1d82e70ef0a2b9112c7e25311690eaacfaede6
= <gmedina.santos@gmail.com>
2022-03-13 15:46:47 -0400

    feat: add pages document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class StoreDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default StoreDocument
