8f2eba5b5ae033c7bf01211bbcc74230e9c39476
= <gmedina.santos@gmail.com>
2022-03-13 15:48:11 -0400

    feat: add cart icon

---
+++ src/components/icons/Cart/Cart.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { TIcon } from '../types'
+
+export const IconCart: React.FC<TIcon & React.HTMLAttributes<any>> = ({
+  color = '#303030',
+  width = 24,
+  height = 24,
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      viewBox="0 0 512 512"
+    >
+      <title>Cart</title>
+      <circle
+        cx="176"
+        cy="416"
+        r="16"
+        fill="none"
+        stroke={color}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="32"
+      />
+      <circle
+        cx="400"
+        cy="416"
+        r="16"
+        fill="none"
+        stroke={color}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="32"
+      />
+      <path
+        fill="none"
+        stroke={color}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="32"
+        d="M48 80h64l48 272h256"
+      />
+      <path
+        d="M160 288h249.44a8 8 0 007.85-6.43l28.8-144a8 8 0 00-7.85-9.57H128"
+        fill="none"
+        stroke={color}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="32"
+      />
+    </svg>
+  )
+}
---
+++ src/components/icons/types.ts
@@ -0,0 +1,5 @@
+export type TIcon = {
+  color?: string
+  width?: number
+  height?: number
+}
