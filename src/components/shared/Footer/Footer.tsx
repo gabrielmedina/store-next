08e28d0c027793922a22eae1b8e27d55ce06a4fc
Gabriel Medina <gmedina.santos@gmail.com>
2022-06-06 20:27:57 -0400

    feat: add footer styles

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,10 +1,15 @@
+import { Logo } from 'src/components'
 import styles from './Footer.module.scss'
 
 export const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <h2>Store</h2>
-      <small>The best since 2021. 10001 - New York</small>
+      <h2 className={styles.logo}>
+        <Logo />
+      </h2>
+      <small className={styles.copyright}>
+        The best since 2021. 10001 - New York
+      </small>
     </footer>
   )
 }
