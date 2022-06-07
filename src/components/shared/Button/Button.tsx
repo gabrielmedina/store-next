cd4d9efeef67e19530bf3b3a90c1324245568490
Gabriel Medina <gmedina.santos@gmail.com>
2022-06-06 22:14:24 -0400

    feat: add rounded and fullWidth props

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -4,14 +4,19 @@ import styles from './Button.module.scss'
 
 type TButtonProps = {
   variant?: 'primary' | 'secondary'
+  rounded?: boolean
+  fullWidth?: boolean
 }
 
 export const Button: React.FC<
   TButtonProps & React.ButtonHTMLAttributes<HTMLElement>
-> = ({ variant = 'primary', onClick, children }) => {
+> = ({ variant = 'primary', rounded, fullWidth, onClick, children }) => {
   return (
     <button
-      className={classNames(styles.button, styles[variant])}
+      className={classNames(styles.button, styles[variant], {
+        [styles.rounded]: rounded,
+        [styles.fullWidth]: fullWidth,
+      })}
       onClick={onClick}
     >
       {children}
