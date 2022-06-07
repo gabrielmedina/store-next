1f7a306c4736c3ca64a65f1337905a7b738ed458
Gabriel Medina <gmedina.santos@gmail.com>
2022-06-06 20:28:18 -0400

    chore: export container component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,11 @@
 export * from './layout/Default/Default'
+export * from './layout/shared/Container/Container'
 
 export * from './shared/Header/Header'
 export * from './shared/Footer/Footer'
 export * from './shared/Logo/Logo'
 export * from './shared/Button/Button'
+export * from './shared/Card/Card'
 
 export * from './icons/Cart/Cart'
+
