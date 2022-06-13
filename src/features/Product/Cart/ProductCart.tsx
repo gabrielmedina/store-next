927e9f7ff0d5c06278534fff737da6eeee65953d
Gabriel Medina <gmedina.santos@gmail.com>
2022-06-12 21:34:54 -0400

    feat: add checkout button

--- src/features/Product/Cart/ProductCart.tsx
+++ src/features/Product/Cart/ProductCart.tsx
@@ -1,13 +1,22 @@
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { ProductCartList, StateCartOpen, StateCartItems } from 'src/features'
-import { Dialog } from 'src/components'
+import { Button, Dialog } from 'src/components'
 
 export const ProductCart: React.FC = () => {
   const cartItems = useRecoilValue(StateCartItems)
   const [cartOpen, setCartOpen] = useRecoilState(StateCartOpen)
 
   return (
-    <Dialog open={cartOpen} title="My cart" onClose={() => setCartOpen(false)}>
+    <Dialog
+      open={cartOpen}
+      title="My cart"
+      onClose={() => setCartOpen(false)}
+      footer={
+        <Button fullWidth onClick={() => alert('Go to checkout')}>
+          Go to checkout
+        </Button>
+      }
+    >
       {<ProductCartList products={cartItems} />}
     </Dialog>
   )
