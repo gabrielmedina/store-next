dc0c4eae95d1649c43c60cac92c9904de7393662
Gabriel Medina <gmedina.santos@gmail.com>
2022-06-16 20:15:15 -0400

    chore: update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,19 @@
 import nextJest from 'next/jest'
 
 const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 })
 
 const customJestConfig = {
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  moduleDirectories: ['node_modules', 'src'],
+  moduleDirectories: ['node_modules', __dirname],
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
+    'index.ts', // Ignoring component index since it is used only for importing
   ],
   transform: {
     '^.+\\.tsx?$': ['@swc/jest'],
@@ -26,8 +29,7 @@ const customJestConfig = {
   coveragePathIgnorePatterns: [
     'src/pages/_app.tsx',
     'src/pages/_error.tsx',
-    'src/pages/_document.tsx',
-    'index.ts'
+    'src/pages/_document.tsx'
   ],
   moduleNameMapper: {
     '\\.(scss|css)$': 'identity-obj-proxy',
