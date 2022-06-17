aae5ef5296b66da3c1fcd5109b79c068d16090b0
Gabriel Medina <gmedina.santos@gmail.com>
2022-06-16 20:17:00 -0400

    feat: add increase count method

--- src/features/Product/Cart/Item/ProductCartItem.tsx
+++ src/features/Product/Cart/Item/ProductCartItem.tsx
@@ -1,16 +1,23 @@
 import Image from 'next/image'
+import { useState } from 'react'
 import { Button, IconAdd, IconRemove } from 'src/components'
 import { TProduct } from 'src/features'
 import { formatyMoney } from 'src/lib'
 import styles from './ProductCartItem.module.scss'
 
-type TProductCartItemProps = {
+export type TProductCartItemProps = {
   product?: TProduct
 }
 
 export const ProductCartItem: React.FC<TProductCartItemProps> = ({
   product,
 }) => {
+  const [count, setCount] = useState(1)
+
+  const increase = () => {
+    setCount(count + 1)
+  }
+
   return product ? (
     <section className={styles.container}>
       <figure className={styles.figure}>
@@ -32,8 +39,15 @@ export const ProductCartItem: React.FC<TProductCartItemProps> = ({
           <Button variant="secondary" rounded className={styles.buttonClose}>
             <IconRemove />
           </Button>
-          <span className={styles.count}>1</span>
-          <Button variant="secondary" rounded className={styles.buttonClose}>
+          <span data-testid="count" className={styles.count}>
+            {count}
+          </span>
+          <Button
+            variant="secondary"
+            rounded
+            className={styles.buttonClose}
+            onClick={increase}
+          >
             <IconAdd />
           </Button>
         </div>
