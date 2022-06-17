418b8cbcf8074776b0e034446092672b1fde71ab
Gabriel Medina <gmedina.santos@gmail.com>
2022-06-16 21:19:59 -0400

    feat: no render when products is not provided

--- src/features/Product/Cart/List/ProductCartList.tsx
+++ src/features/Product/Cart/List/ProductCartList.tsx
@@ -2,21 +2,22 @@ import { TProduct } from 'src/features/Product/types'
 import { ProductCartItem } from '../Item/ProductCartItem'
 import styles from './ProductCartList.module.scss'
 
-type TProductCartListProps = {
+export type TProductCartListProps = {
   products?: Array<TProduct>
 }
 
 export const ProductCartList: React.FC<TProductCartListProps> = ({
   products,
 }) => {
+  if (!products?.length) return null
+
   return (
     <ol className={styles.list}>
-      {products &&
-        products.map((product) => (
-          <li className={styles.item} key={product.id}>
-            <ProductCartItem product={product} />
-          </li>
-        ))}
+      {products?.map((product) => (
+        <li className={styles.item} key={product.id}>
+          <ProductCartItem product={product} />
+        </li>
+      ))}
     </ol>
   )
 }
