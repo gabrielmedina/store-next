52f433251aabfb923f56644dc9539118790e6e6d
Gabriel Medina <gmedina.santos@gmail.com>
2022-07-02 15:15:13 -0400

    feat: container sizes

--- src/components/layout/shared/Container/Container.tsx
+++ src/components/layout/shared/Container/Container.tsx
@@ -1,5 +1,15 @@
+import classNames from 'classnames'
 import styles from './Container.module.scss'
 
-export const Container: React.FC = ({ children }) => {
-  return <div className={styles.container}>{children}</div>
+type TContainerProps = {
+  size?: 'small' | 'medium'
+}
+
+export const Container: React.FC<TContainerProps> = ({
+  children,
+  size = 'medium',
+}) => {
+  return (
+    <div className={classNames(styles.container, styles[size])}>{children}</div>
+  )
 }
