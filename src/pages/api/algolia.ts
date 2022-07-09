0e7841dc60b49c6e62df05f34739c1baed73a03a
Gabriel Medina <gmedina.santos@gmail.com>
2022-07-09 13:38:20 -0400

    feat: algolia partial update objects

--- src/pages/api/algolia.ts
+++ src/pages/api/algolia.ts
@@ -1,26 +1,34 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { request } from 'graphql-request'
-import { GET_PRODUCTS_QUERY } from 'src/graphql'
+import { GET_PRODUCT_BY_ID } from 'src/graphql'
 import algoliasearch from 'algoliasearch'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const Algolia = algoliasearch(
+    process.env.ALGOLIA_APPLICATION_ID!,
+    process.env.ALGOLIA_API_ADMIN_KEY!
+  )
+  const algoliaClient = Algolia.initIndex('dev_store')
+
   try {
-    const response = await request(process.env.GRAPHCMS_CONTENT_API!, GET_PRODUCTS_QUERY)
+    const product = await getProductFromGraphCMS(req.body.data.id)
+    const oldProduct = await algoliaClient.search(req.body.data.id)
 
-    const algolia = algoliasearch(
-      process.env.ALGOLIA_APPLICATION_ID!,
-      process.env.ALGOLIA_API_ADMIN_KEY!
-    )
-    const client = algolia.initIndex('dev_store')
-
-    await client.clearObjects()
-
-    await client.saveObjects(response.products, {
-      autoGenerateObjectIDIfNotExist: true
+    await algoliaClient.partialUpdateObjects({
+      ...product,
+      objectID: oldProduct.hits[0].objectID
     })
 
     res.send(200)
   } catch (error) {
     res.status(400).send(error)
   }
+}
+
+async function getProductFromGraphCMS(productId: string) {
+  return await request(process.env.GRAPHCMS_CONTENT_API!, GET_PRODUCT_BY_ID, {
+    variables: {
+      id: productId
+    }
+  })
 }
