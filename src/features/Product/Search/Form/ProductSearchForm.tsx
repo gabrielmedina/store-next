e1272b9ac4436f6dfd237d4c64fe36cc93779e60
Gabriel Medina <gmedina.santos@gmail.com>
2022-07-13 00:00:39 -0400

    feat: update product search list with search form submit

--- src/features/Product/Search/Form/ProductSearchForm.tsx
+++ src/features/Product/Search/Form/ProductSearchForm.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { useSetRecoilState } from 'recoil'
+import { getAlgoliaClient } from 'src/lib'
+import { StateSearchItems } from '../SearchState'
 import styles from './ProductSearchForm.module.scss'
 
 type TProductSearchFormData = {
@@ -10,8 +13,14 @@ export const ProductSearchForm: React.FC<React.HTMLAttributes<any>> = ({
   ...props
 }) => {
   const { register, handleSubmit } = useForm<TProductSearchFormData>()
+  const setStateSearchItems = useSetRecoilState(StateSearchItems)
+  const algoliaClient = getAlgoliaClient({ index: 'dev_store' })
 
-  const onSubmit = handleSubmit((data) => console.log(data))
+  const onSubmit = handleSubmit(async ({ search }) => {
+    const { hits } = await algoliaClient.search(search)
+    // @ts-ignore: Unreachable code error
+    setStateSearchItems(hits)
+  })
 
   return (
     <form
