7d31756d464004f6de0e74edf7ccc574c0e3d7b8
Gabriel Medina <gmedina.santos@gmail.com>
2023-01-23 14:37:39 -0400

    feat: add fetch products by category from algolia

---
+++ src/features/Product/api/fetchProductsByCategoryFromAlgolia.ts
@@ -0,0 +1,47 @@
+import { getAlgoliaClient } from 'src/libs'
+import { ParsedUrlQuery } from 'querystring'
+
+type TFetchProductsFromAlgolia = {
+  query: ParsedUrlQuery
+}
+
+export const fetchProductsByCategoryFromAlgolia = async ({
+  query,
+}: TFetchProductsFromAlgolia) => {
+  const algoliaClient = getAlgoliaClient({
+    index: 'dev_store',
+  })
+
+  algoliaClient.setSettings({
+    attributesForFaceting: [
+      'searchable(category.slug)',
+    ]
+  })
+
+  const querySearch = (query?.search as string) || ''
+  const queryPagination = query?.page ? parseInt(query.page as string) - 1 : 0
+
+  const {
+    hits,
+    nbHits,
+    nbPages,
+    page: currentPage,
+    query: term,
+  } = await algoliaClient.search(querySearch, {
+    page: queryPagination,
+    filters: `category.slug: ${query.category}`
+  })
+
+  return {
+    loading: false,
+    term,
+    products: {
+      total: nbHits,
+      data: hits,
+    },
+    pages: {
+      total: nbPages,
+      current: currentPage,
+    },
+  }
+}
