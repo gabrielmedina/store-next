835cd9bb068ac4dc76dd42c676d0822c383d5b11
Gabriel Medina <gmedina.santos@gmail.com>
2023-01-25 13:31:03 -0400

    test: product search list headline

--- src/features/Product/components/Search/List/ProductSearchList.test.tsx
+++ src/features/Product/components/Search/List/ProductSearchList.test.tsx
@@ -6,8 +6,8 @@ import {
 import { ProductSearchList, TProductSearchListProps } from './ProductSearchList'
 import ProductsStub from 'test/_stubs/ProductsStub.json'
 
-const makeSut = ({ products }: TProductSearchListProps) => {
-  return render(<ProductSearchList products={products} />)
+const makeSut = (props: TProductSearchListProps) => {
+  return render(<ProductSearchList {...props} />)
 }
 
 describe('ProductSearchList', () => {
@@ -22,9 +22,18 @@ describe('ProductSearchList', () => {
   it('should render correctly', () => {
     const products = ProductsStub
     // @ts-ignore
-    makeSut({ products })
+    makeSut({ products, total: products.length })
 
     expect(screen.getByRole('list')).toBeInTheDocument()
+    expect(screen.getByText(/All products/)).toBeInTheDocument()
     expect(screen.getAllByRole('listitem')).toHaveLength(products.length)
+  })
+
+  it('should render with headline', () => {
+    const products = ProductsStub
+    // @ts-ignore
+    makeSut({ products, total: products.length, headline: 'caps' })
+
+    expect(screen.getByText(/Search results for: caps/)).toBeInTheDocument()
   })
 })
